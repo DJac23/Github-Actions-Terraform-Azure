0814ac64406b041b0b9281b7b15729b003255c75
D. Jackson <dorion.jackson@corebts.com>
2022-02-21 13:16:19 -0500

    init

---
+++ Github-Actions-Terraform-Azure/terraform/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "new-grp"
+    storage_account_name = "terraformstate2162022"
+    container_name       = "tfstatefile"
+    key                  = "dev.terraform.tfstate"
+  }
+}
+module "RG" {
+  source   = "./modules/RG" #A
+  rgname   = var.rgname     
+  location = var.location
+}
+module "SA" {
+  source   = "./modules/StorageAccount"
+  sname    = var.sname
+  rgname   = var.rgname
+  location = var.location
+}
