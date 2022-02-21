4d8bceb9c823eff9345f7db5fa981e12e713dc5b
D. Jackson <dorion.jackson@corebts.com>
2022-02-21 15:10:56 -0500

    init

--- Github-Actions-Terraform-Azure/terraform/terraform.tfvars
+++ Github-Actions-Terraform-Azure/terraform/terraform.tfvars
@@ -1,3 +1,7 @@
 rgname   = "github-action"
 location = "East US"
 sname    = "devopsguru190"
+instance_tags = {
+    name = "github-action"
+    env = "Dev"
+}
--- Github-Actions-Terraform-Azure/terraform/variables.tf
+++ Github-Actions-Terraform-Azure/terraform/variables.tf
@@ -1,14 +1,20 @@
 variable "rgname" {
   description = "Resource Group Name"
-  default     = "github-action1"
   type        = string
 }
 variable "location" {
   description = "Azure location"
-  default     = "East US"
   type        = string
 }
 variable "sname" {
   description = "Azure Storage Account"
   type        = string
+}
+
+variable "instance_tags" {
+  type = object ({
+    Name = String
+    Env = String
+  })
+ 
 }
