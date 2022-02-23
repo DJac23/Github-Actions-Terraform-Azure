78d49fa8424c2486d4f668a73c8f95f736734b98
D. Jackson <dorion.jackson@corebts.com>
2022-02-23 16:53:52 -0500

    new

--- Github-Actions-Terraform-Azure/terraform/main.tf
+++ Github-Actions-Terraform-Azure/terraform/main.tf
@@ -14,8 +14,8 @@ module "RG" {
 resource "azurerm_virtual_network" "github-action" {
   name                = "github-action-network"
   address_space       = ["10.0.0.0/16"]
-  location            = module.RG.rgname.location
-  resource_group_name = module.RG.rgname.name
+  location            = module.RG.rg.location
+  resource_group_name = module.RG.rg.name
 }
 resource "azurerm_subnet" "github-action" {
   name                 = "internal"
