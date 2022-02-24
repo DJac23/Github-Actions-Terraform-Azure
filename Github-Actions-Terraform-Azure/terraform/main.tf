4834f42454c905f6f079bc4bf62212f00106d557
D. Jackson <dorion.jackson@corebts.com>
2022-02-24 13:18:10 -0500

    destory

--- Github-Actions-Terraform-Azure/terraform/main.tf
+++ Github-Actions-Terraform-Azure/terraform/main.tf
@@ -17,58 +17,58 @@ resource "azurerm_virtual_network" "github-action" {
   location            = var.location
   resource_group_name = module.RG.resourcegroup_name.rg_name
 }
-resource "azurerm_subnet" "github-action-subnet" {
-  subnet_name          = var.subnet_name[count.index]
-  resource_group_name  = module.RG.resourcegroup_name.rg_name
-  virtual_network_name = azurerm_virtual_network.github-action.name
-  address_prefixes     = var.address_prefixes[count.index]
-  count = 2
+# resource "azurerm_subnet" "github-action-subnet" {
+#   subnet_name          = var.subnet_name[count.index]
+#   resource_group_name  = module.RG.resourcegroup_name.rg_name
+#   virtual_network_name = azurerm_virtual_network.github-action.name
+#   address_prefixes     = var.address_prefixes[count.index]
+#   count = 2
 
-}
+# }
 
-resource "azurerm_network_interface" "github-action" {
-  location            = var.location
-  resource_group_name = module.RG.resourcegroup_name.rg_name
-  count = 2
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.github-action.id
-    private_ip_address_allocation = "Dynamic"
-  }
-  tags = {
-    Name = "github-action-nic ${count.index}"
-    Env = "Dev"    
-}
+# resource "azurerm_network_interface" "github-action" {
+#   location            = var.location
+#   resource_group_name = module.RG.resourcegroup_name.rg_name
+#   count = 2
+#   ip_configuration {
+#     name                          = "internal"
+#     subnet_id                     = azurerm_subnet.github-action.id
+#     private_ip_address_allocation = "Dynamic"
+#   }
+#   tags = {
+#     Name = "github-action-nic ${count.index}"
+#     Env = "Dev"    
+# }
 
-}
-resource "azurerm_windows_virtual_machine" "github-action" {
-  resource_group_name = module.RG.resourcegroup_name.rg_name
-  location            = var.location
-  size                = "Standard_F2"
-  admin_username      = var.admin_username
-  admin_password      = var.admin_password
-  count = 2
-  network_interface_ids = [
-    azurerm_network_interface.github-action[count.index]
-  ]
-  
-  tags = {
-    Name = "github-action ${count.index}"
-    Env = "Dev"
-}
+# }
+# resource "azurerm_windows_virtual_machine" "github-action" {
+#   resource_group_name = module.RG.resourcegroup_name.rg_name
+#   location            = var.location
+#   size                = "Standard_F2"
+#   admin_username      = var.admin_username
+#   admin_password      = var.admin_password
+#   count = 2
+#   network_interface_ids = [
+#     azurerm_network_interface.github-action[count.index]
+#   ]
 
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
+#   tags = {
+#     Name = "github-action ${count.index}"
+#     Env = "Dev"
+# }
 
-  source_image_reference {
-    publisher = "MicrosoftWindowsServer"
-    offer     = "WindowsServer"
-    sku       = "2016-Datacenter"
-    version   = "latest"
-  }
-}
+#   os_disk {
+#     caching              = "ReadWrite"
+#     storage_account_type = "Standard_LRS"
+#   }
+
+#   source_image_reference {
+#     publisher = "MicrosoftWindowsServer"
+#     offer     = "WindowsServer"
+#     sku       = "2016-Datacenter"
+#     version   = "latest"
+#   }
+# }
 module "SA" {
   source   = "./modules/StorageAccount"
   sname    = var.sname
--- Github-Actions-Terraform-Azure/terraform/terraform.tfvars
+++ Github-Actions-Terraform-Azure/terraform/terraform.tfvars
@@ -1,6 +1,6 @@
 rgname   = "github-action"
 location = "East US"
 sname    = "devopsguru190"
-admin_username = "adminuser"
-admin_password = "P@$$w0rd1234!"
-Subnet_name   = ["github-action-subnet1", "github-action-subnet2"]
+# admin_username = "adminuser"
+# admin_password = "P@$$w0rd1234!"
+# Subnet_name   = ["github-action-subnet1", "github-action-subnet2"]
