7158adbd7582d5b58d4f8d4b432647921c8184d6
D. Jackson <dorion.jackson@corebts.com>
2022-02-25 11:43:38 -0500

    push

--- Github-Actions-Terraform-Azure/terraform/main.tf
+++ Github-Actions-Terraform-Azure/terraform/main.tf
@@ -35,7 +35,7 @@ resource "azurerm_network_interface" "github-action-nic" {
   count = 2
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = "${azurerm_subnet.github-action-subnet.id} e${count.index}"
+    subnet_id                     = "${azurerm_subnet.github-action-subnet[count.idex].id}"
     private_ip_address_allocation = "Dynamic"
   }
   
