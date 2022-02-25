157fe666737db92fc8a30d39fffd2f7533aae875
D. Jackson <dorion.jackson@corebts.com>
2022-02-25 10:25:01 -0500

    PUSH20

--- Github-Actions-Terraform-Azure/terraform/variables.tf
+++ Github-Actions-Terraform-Azure/terraform/variables.tf
@@ -17,7 +17,7 @@ variable "subnet_name" {
 
 variable "address_prefixes" {
   description = "Subnet Prefix"
-  type        = tuple
+  default        = ["10.0.2.0/24","10.0.1.0/.24"]
 }
 
 variable "admin_username" {
