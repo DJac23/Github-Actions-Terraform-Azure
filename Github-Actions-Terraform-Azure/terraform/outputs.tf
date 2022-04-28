9562129b900526f6573bbb4a7d37108fb813db41
D. Jackson <dorion.jackson@corebts.com>
2022-04-28 13:53:48 -0400

    destroy

--- Github-Actions-Terraform-Azure/terraform/outputs.tf
+++ Github-Actions-Terraform-Azure/terraform/outputs.tf
@@ -1,29 +1,29 @@
-# output "rg_name" {
-#   value = {
-#     appname = module.RG.resourcegroup_name.rg_name
-#   }
+# # output "rg_name" {
+# #   value = {
+# #     appname = module.RG.resourcegroup_name.rg_name
+# #   }
+# # }
+# # output "adf_id" {
+# #   value = data.azurerm_data_factory.adf.id
+# # }
+
+# output "subnet_id" {
+#   value = data.azurerm_subnet.subnet.id
 # }
-# output "adf_id" {
-#   value = data.azurerm_data_factory.adf.id
+
+# output "subnet_id_01" {
+#   value = data.azurerm_subnet.subnet1.id
 # }
 
-output "subnet_id" {
-  value = data.azurerm_subnet.subnet.id
-}
+# output "rg_id" {
+#   value = data.azurerm_resource_group.name.id
+# }
 
-output "subnet_id_01" {
-  value = data.azurerm_subnet.subnet1.id
-}
+# output "virtual_network_id" {
+#   value = data.azurerm_virtual_network.vnet.id
+# }
 
-output "rg_id" {
-  value = data.azurerm_resource_group.name.id
-}
-
-output "virtual_network_id" {
-  value = data.azurerm_virtual_network.vnet.id
-}
-
-output "sql_server_id" {
-  value = data.azurerm_mssql_server.sqlserver.id
-}
+# output "sql_server_id" {
+#   value = data.azurerm_mssql_server.sqlserver.id
+# }
 
