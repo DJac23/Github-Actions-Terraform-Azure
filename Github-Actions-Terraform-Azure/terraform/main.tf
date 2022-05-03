0efc480f507ea41e561783e933456192250e4bc2
D. Jackson <dorion.jackson@corebts.com>
2022-05-03 18:43:20 -0400

    twelve

--- Github-Actions-Terraform-Azure/terraform/main.tf
+++ Github-Actions-Terraform-Azure/terraform/main.tf
@@ -262,7 +262,7 @@ resource "azurerm_linux_virtual_machine" "linuxvm" {
   admin_username = var.admin_username 
   admin_password = var.admin_password
   
-  custom_data = filebase64("customdata.tpl")
+  custom_data = filebase64("ip_forward.sh")
 
    os_disk {
     name                 = "myOsDisk${count.index}"
